7ecd729313ed5d232947f97a89da8805fbbe07fe
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2018-07-04 21:32:01 +0200

    Add data access and data model projects. Separate CSVHelper mapping to its own project.

---
+++ GravityVectorToolKit.DataAccess/GravityVectorDataContext.cs
@@ -0,0 +1,18 @@
+﻿using GravityVectorToKML;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GravityVectorToolKit.DataAccess
+{
+    public class GravityVectorDataContext : DbContext
+    {
+        public DbSet<NormalPointG> NormalPoints { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(@"Database=GravityVectorToolKit;Data Source=localhost;User Id=gvtk;Password=gvtk;");
+        }
+    }
+}
