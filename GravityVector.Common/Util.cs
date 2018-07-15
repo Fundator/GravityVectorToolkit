22366a103060d2f16aa59a6e61387022f14b019b
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2018-07-15 21:30:52 +0200

    Refactoring, update Log4net config, add database import project. Database import correctly creates the schema, however creating the session factory fails due to a bug in the MySQL data driver (see issue 86974 in the MySQL bug tracker).

---
+++ GravityVector.Common/Util.cs
@@ -0,0 +1,24 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using GravityVectorToKML.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace GravityVector.Common
+{
+    public class Util
+    {
+        public static List<NormalPoint> ReadGravityVector(string file)
+        {
+            var csv = new CsvReader(File.OpenText(file), new Configuration
+            {
+                CultureInfo = CultureInfo.InvariantCulture
+            });
+            var records = csv.GetRecords<NormalPoint>().ToList();
+            return records;
+        }
+    }
+}
