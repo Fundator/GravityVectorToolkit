29f4f5485b7defc7b2b1d1303897fa01407a8114
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2019-02-17 23:14:53 +0100

    Refactor and update NuGet packages.

--- GravityVectorToolKit.Mapping/NormalPointCsvClassMap.cs
+++ GravityVectorToolKit.Mapping/NormalPointCsvClassMap.cs
@@ -8,23 +8,6 @@ using System.Globalization;
 
 namespace GravityVectorToolKit.CSV.Mapping
 {
-	public class NormalRouteCsvClassMap : ClassMap<NormalRoute>
-	{
-		private WKTReader wktReader = new WKTReader();
-
-		public NormalRouteCsvClassMap()
-		{
-			//Map(m => m.NormalRouteId).Name("");
-			Map(m => m.FromLocationId).Name("dep_id");
-			Map(m => m.ToLocationId).Name("arr_id");
-			Map(m => m.NormalRouteGeometry).ConvertUsing(row =>
-			{
-				var p = wktReader.Read(row.GetField("normal_route"));
-				p.SRID = 4326;
-				return p;
-			});
-		}
-	}
 
 	public class NormalPointCsvClassMap : ClassMap<NormalPoint>
 	{
