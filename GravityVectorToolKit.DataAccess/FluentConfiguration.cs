b493e367d937b26649f8af4bd6751c5b465eb41a
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2019-02-17 22:24:01 +0100

    Cleanup fluent configuration.

--- GravityVectorToolKit.DataAccess/FluentConfiguration.cs
+++ GravityVectorToolKit.DataAccess/FluentConfiguration.cs
@@ -1,18 +1,11 @@
 ﻿using FluentNHibernate.Cfg;
-using FluentNHibernate.Conventions.Helpers;
 using GravityVectorToolKit.CSV.Mapping;
-using GravityVectorToolKit.DataModel;
 using NHibernate;
 using NHibernate.Driver;
 using NHibernate.Spatial.Dialect;
 using NHibernate.Spatial.Mapping;
 using NHibernate.Spatial.Type;
 using NHibernate.Tool.hbm2ddl;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DemoDataAccess
 {
@@ -20,18 +13,12 @@ namespace DemoDataAccess
 	{
 		public static void Configure(string connectionString, bool generateTables = false)
 		{
-
 			var cfg = Fluently.Configure()
-				//.Database(FluentNHibernate.Cfg.Db.MySQLConfiguration.Standard
-				//.ConnectionString("Server=localhost;Database=gravityvectortoolkit;Connect Timeout=36000;port=3306;Uid=gvtk;Pwd=gvtk")
-				//.Driver<MySqlDataDriver>()
-				//.Dialect<MySQL57SpatialDialect>())
-				// Server=localhost;Port=5432;Database=gvtk20181230;User Id=gvtk;Password = gvtk;
 				.Database(FluentNHibernate.Cfg.Db.PostgreSQLConfiguration.Standard
 				.ConnectionString(connectionString)
 				.Driver<NpgsqlDriver>()
 				.Dialect<PostGis20Dialect>())
-				.Mappings(x => 
+				.Mappings(x =>
 					{
 						x.FluentMappings.Add(typeof(NormalPointGMapping<PostGisGeometryType>));
 						x.FluentMappings.Add(typeof(NormalRouteMapping<PostGisGeometryType>));
@@ -50,25 +37,20 @@ namespace DemoDataAccess
 			}
 
 			SessionManager.SessionFactory = cfg.BuildSessionFactory();
-
 		}
 	}
-
 
 	public static class SessionManager
 	{
 		private static ISession _session;
 		public static ISessionFactory SessionFactory;
+
 		public static ISession Session
 		{
 			get
 			{
 				if (_session == null)
 				{
-					//if (SessionFactory == null)
-					//{
-					//	FluentConfiguration.Configure();
-					//}
 					_session = SessionFactory.OpenSession();
 				}
 				return _session;
